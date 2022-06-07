a9c9af1de761952c327b51670284e9e9035aead3
Luis Sáenz <luis.saenz.software@gmail.com>
2022-06-07 17:28:37 -0600

    Ver por paises

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+import { switchMap, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-pais.component.css']
 })
 export class VerPaisComponent implements OnInit {
+  pais! : Country [];
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute, private paisService:PaisService) { }
 
   ngOnInit(): void {
+// this.activateRoute.params.pipe(
+//   switchMap(({id}) => this.paisService.getPaisPorCodigo(id)),
+//   tap(console.log)).
+//   subscribe(pais => this.pais = pais
+    
+//   });
+
+
+
+
+  this.activateRoute.params.subscribe(({id}) => {
+  this.paisService.getPaisPorCodigo(id).subscribe(pais => {
+    this.pais = pais;
+  console.log(this.pais);
+  });
+  
+  });
   }
 
+  
 }
