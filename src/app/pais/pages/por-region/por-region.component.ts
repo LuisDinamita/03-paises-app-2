6df65e644565646b8d2dcc20e79139e9bb989dec
Luis Sáenz <luis.saenz.software@gmail.com>
2022-06-08 15:31:03 -0600

    corrección de errores

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./por-region.component.css']
 })
 export class PorRegionComponent implements OnInit {
+  
+  
+  regiones :string []= ['africa','americas','asia','europe','oceania'];
+  regionActiva: string ="";
+  paises : Country[]=[];
+  
+ 
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
+
+  getClassCss(region: string){
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string){
+    
+    if(region === this.regionActiva ){return}
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService.getRegion(region).subscribe((paises)=>{
+      this.paises = paises; 
+    });
+  }
+
+  
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient   } from '@angular/common/http';
+import { HttpClient, HttpParams   } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
@@ -7,26 +7,41 @@ import { Country } from '../interfaces/pais.interface';
   providedIn: 'root'
 })
 export class PaisService {
+  buscarRegion(region: string) {
+    throw new Error('Method not implemented.');
+  }
+ 
+ 
     
 
   private _apiUrl: string = 'https://restcountries.com/v3.1';
+  
+  get params() {
+    return new HttpParams().set('fields','name,capital,cca2,flags,population');
+  }
+
   constructor (private http: HttpClient) { }
 
   buscarPais(termino: string): Observable <Country[]> {
     const url = `${this._apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.params});
   }
 
   buscarCapital(termino: string): Observable <Country[]> {
     const url = `${this._apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params: this.params});
   }
 
   getPaisPorCodigo (id:string):Observable<Country[]>{
     const url = `${this._apiUrl}/alpha/${id}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,);
   }
-    
+  getRegion (region:string):Observable<Country[]>{
+    // const params = new HttpParams().set('fields','name,capital,cca2,flags,population')
+    const url = `${this._apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url,{params: this.params});
+  }
+  
   }
 
 
